1015e6293f49032800adaf61566c94e735f0c7b0
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-12-20 09:26:12 +0800

    try adding images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 module.exports = {
+  images: {
+    imageSizes: [18, 36, 54, 24, 48, 72, 96, 144],
+    domains: [
+      "dl.airtable.com",
+      "emoji.slack-edge.com",
+      "cloud-lp0r5yk68.vercel.app",
+    ],
+  },
   async rewrites() {
     return [
       {
-        "source": "/",
-        "destination": "/api"
-      }
-    ]
+        source: "/",
+        destination: "/api",
+      },
+    ];
   },
-}
+};
